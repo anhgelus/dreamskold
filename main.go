a4989188170c7ab99ce6265998090cb8142be8fc
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@proton.me>
2024-07-19 15:13:06 +0200

    feat(gokord): setup gokord

---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	_ "embed"
+	"flag"
+	"github.com/anhgelus/gokord"
+	"github.com/bwmarrin/discordgo"
+)
+
+var (
+	token string
+	//go:embed updates.json
+	updatesData []byte
+	Version     = gokord.Version{
+		Major: 0,
+		Minor: 0,
+		Patch: 0,
+	} // git version: 0.0.0
+)
+
+func init() {
+	flag.StringVar(&token, "token", "", "token of the bot")
+	flag.Parse()
+}
+
+func main() {
+	err := gokord.SetupConfigs([]*gokord.ConfigInfo{})
+	if err != nil {
+		panic(err)
+	}
+
+	err = gokord.DB.AutoMigrate()
+	if err != nil {
+		panic(err)
+	}
+
+	innovations, err := gokord.LoadInnovationFromJson(updatesData)
+	if err != nil {
+		panic(err)
+	}
+
+	adm := gokord.AdminPermission
+
+	bot := gokord.Bot{
+		Token: token,
+		Status: []*gokord.Status{
+			{
+				Type:    gokord.WatchStatus,
+				Content: "DreamSköld",
+			},
+			{
+				Type:    gokord.GameStatus,
+				Content: "être dev par @anhgelus",
+			},
+			{
+				Type:    gokord.ListeningStatus,
+				Content: "http 418, I'm a tea pot",
+			},
+			{
+				Type:    gokord.GameStatus,
+				Content: "DreamSköld " + Version.String(),
+			},
+		},
+		Commands:    nil,
+		AfterInit:   afterInit,
+		Version:     nil,
+		Innovations: innovations,
+		Name:        "DreamSköld",
+	}
+	bot.Start()
+}
+
+func afterInit(dg *discordgo.Session) {
+
+}
